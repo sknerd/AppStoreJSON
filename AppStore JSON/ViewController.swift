6930c8a71ae13d1b4f72048cea9b8616bbef9521
Renald <renksss@gmail.com>
2019-11-27 12:20:52 +0300

    Initial Commit

---
+++ AppStore JSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStore JSON
+//
+//  Created by renks on 27.11.2019.
+//  Copyright © 2019 Renald Renks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
